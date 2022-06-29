304bb126b319e1148bf240cd469b6b1e7043ea19
Abdullah <Abdullah@WIN-84BVOTR7HBP>
2022-06-29 05:13:19 +0500

    Add project files.

---
+++ assign1/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace assign1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+    }
+}
